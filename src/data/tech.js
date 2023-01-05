8785e7a046efd25ffe8d0f5c4ec697aa25c84e23
Joe <>
2023-01-05 18:38:43 +0800

    update image url

--- src/data/tech.js
+++ src/data/tech.js
@@ -1,36 +1,36 @@
 const techRecords = [
   {
-    link: 'http://asset.f18.tech/%E5%9F%BA%E6%9C%AC%E6%90%93%E7%90%831.gif',
+    link: 'http://asset.f18.tech/ji_ben_cuo_qiu1.gif',
     value: 1,
     name: '基本搓球1',
     comment: '大致控制一下撞击的角度，大约30-45度应该都能打到'
   },
   {
-    link: 'http://asset.f18.tech/%E5%9F%BA%E6%9C%AC%E6%90%93%E7%90%832.gif',
+    link: 'http://asset.f18.tech/ji_ben_cuo_qiu2.gif',
     value: 2,
     name: '基本搓球2',
     comment: '控制出球角度大约30度'
   },
   {
-    link: 'http://asset.f18.tech/%E5%9F%BA%E6%9C%AC%E6%90%93%E7%90%832.gif',
+    link: 'http://asset.f18.tech/ji_ben_cuo_qiu3.gif',
     value: 3,
     name: '基本搓球3',
     comment: '控制出球角度大约45~60度'
   },
   {
-    link: 'http://asset.f18.tech/%E4%B8%AD%E6%AF%92%E5%BF%AB%E9%80%9F%E7%90%831.gif',
+    link: 'http://asset.f18.tech/zhong_du_kuai_su_qiu1.gif',
     value: 4,
     name: '中毒快速球1',
     comment: '懒人技巧，中毒以后正常打，不瞄准向后方快速拉杆'
   },
   {
-    link: 'http://asset.f18.tech/%E5%B7%A6%E4%BE%A7%E5%80%92%E9%92%A91.gif',
+    link: 'http://asset.f18.tech/zuo_ce_dao_gou1.gif',
     value: 5,
     name: '左侧倒钩1',
     comment: '左侧墙是垂直的，入射角度和从墙弹出的角度应该是相等的'
   },
   {
-    link: 'http://asset.f18.tech/%E5%B0%8F%E8%A7%92%E5%BA%A6%E9%95%BF%E8%B7%9D%E7%A6%BB%E8%82%89%E6%90%8F1.gif',
+    link: 'http://asset.f18.tech/xiao_jiao_du_chang_ju_li_rou_bo1.gif',
     value: 6,
     name: '小角度长距离肉搏1',
     comment: '大约肉搏后弹出角度为30度，所以撞击时需要控制撞击位置比较薄一点'
